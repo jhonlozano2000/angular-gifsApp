3dddeb513e53bbabc3be855d2c4f98ecaf196969
Jhon Javer Lozano <35960577+jhonlozano2000@users.noreply.github.com>
2022-02-08 21:21:09 -0500

    Primer commit

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+
+
+@NgModule({
+  declarations: [
+    BusquedaComponent,
+    GifsPageComponent,
+    ResultadosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
